41ed93f56585040d5661dc832a2a420c0de3e991
alex <alex@gmail.com>
2025-05-23 21:25:44 -0700

    get data of SECRET_KEY and DEBUG from .env file

--- LibraryManagementSystem/settings.py
+++ LibraryManagementSystem/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-slh&5jm0!-pilf*9n$1(8!z3lo285@z2o&b13pwqa=q#j-+eq%'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG')
 
 ALLOWED_HOSTS = []
 
