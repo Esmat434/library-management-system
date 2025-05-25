c0190b7fd2f4161edd1172684dd63a1ccd11d06a
alex <alex@gmail.com>
2025-05-24 20:32:08 -0700

    created TokenBaseClass abstract amd inheretance this in AccountVerificationToken model

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,9 @@
+from datetime import timedelta
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django_countries.fields import CountryField
 from uuid import uuid4
+from django.utils import timezone
 
 class CustomUser(AbstractUser):
     CHOICE_ROLE = [
@@ -23,7 +25,22 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
 
-class AccountVerificationToken(models.Model):
+class TokenBaseClass(models.Model):
+    def get_default_token_expiration():
+        return timezone.now() + timedelta(hours=24)
+    
     user = models.OneToOneField(CustomUser,on_delete=models.CASCADE)
-    token = models.UUIDField(default=uuid4())
     created_at = models.DateTimeField(auto_now_add=True)
+    expires_at = models.DateTimeField(default=get_default_token_expiration)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return f"This token fro {self.user.username}"
+    
+    def is_expired(self):
+        return timezone.now() >= self.expires_at
+
+class AccountVerificationToken(TokenBaseClass):
+    token = models.UUIDField(default=uuid4)
