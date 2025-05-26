7c4ad2070835cfbaad04e59a7a7b6c5f7dea02f8
alex <alex@gmail.com>
2025-05-25 21:29:49 -0700

    I added the test update user form in test_forms.py

--- accounts/tests/test_forms.py
+++ accounts/tests/test_forms.py
@@ -1,14 +1,12 @@
-from datetime import date
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 from django.core.files.uploadedfile import SimpleUploadedFile
 
 from accounts.forms import (
-    UserForm
+    UserForm,UserUpdateForm
 )
 
-from accounts.models import (
-    CustomUser
-)
+User = get_user_model()
 
 IMAGE_FILE_CONTENT = b'GIF89a\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;'
 
@@ -28,9 +26,9 @@ class TestUserForm(TestCase):
 
         form_data = {
             'username':'ahmad','email':'ahmadi@gmail.com','first_name':'ahmad','last_name':'ahmadi',
-            'phone_number':'+93771230009','address':'timany','city':'kabul',
-            'country':'AF','birth_date':'2000-01-02','password':'Afghan111%',
-            'password1':'Afghan111%'
+            'phone_number':'+93771230009','address':'address','city':'city',
+            'country':'AF','birth_date':'2000-01-02','password':'Test12345%',
+            'password1':'Test12345%'
         }
         file_data = {
             'avatar':avatar_file,
@@ -38,4 +36,33 @@ class TestUserForm(TestCase):
         }
         form = UserForm(form_data,file_data)
         
+        self.assertEqual(form.is_valid(),True)
+
+class TestUpdateUserForm(TestCase):
+    def setUp(self):
+        avatar_file = SimpleUploadedFile(
+            name='test_avatar.gif',
+            content=IMAGE_FILE_CONTENT,
+            content_type='image/gif'
+        )
+        passport_file = SimpleUploadedFile(
+            name='test_passport.gif',
+            content=IMAGE_FILE_CONTENT,
+            content_type='image/gif'
+        )
+        self.user = User.objects.create_user(
+            username = 'test',email = 'test@gmail.com',avatar = avatar_file,passport = passport_file,
+            address = 'address',city = 'city',country = 'AF',birth_date = '2000-03-01',
+            password = 'Test12345%'
+        )
+
+        self.form_data = {
+            'username':'test_ini','email':'test_ini@gmail.com','first_name':'testing','last_name':'testing',
+            'phone_number':'+93771230009','address':'address','city':'city',
+            'country':'AF','birth_date':'2005-01-02'
+        }
+
+    def test_update_user_form_validate(self):
+        form = UserUpdateForm(self.form_data,instance=self.user)
+        print(form.errors)
         self.assertEqual(form.is_valid(),True)
