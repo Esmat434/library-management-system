f4711435eef7aa64613e5cdedb386651443daac8
alex <alex@gmail.com>
2025-05-29 20:24:25 -0700

    add signals in signals.py

--- accounts/signals.py
+++ accounts/signals.py
@@ -1,5 +1,9 @@
+from django.contrib.auth.signals import user_login_failed
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.core.cache import cache
+from django.conf import settings
+import time
 
 from .email_config import send_verification_mail
 
@@ -26,3 +30,37 @@ def send_email_change_password_token(sender,created,instance,**kwargs):
 def send_email_forgot_password_token(sender,created,instance,**kwargs):
     
     send_verification_mail(instance.user.email,'Forgot Password Verification Link',instance.token)
+
+def get_client_ip_from_request(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
+
+@receiver(user_login_failed)
+def user_login_failed_receiver(sender,credentials,request,**kwargs):
+    if not request:
+        return
+    
+    ip_address = get_client_ip_from_request(request)
+    username = credentials.get('username')
+
+    if not username:
+        return 
+    
+    attempt_count_key = f'login_attempts_{ip_address}_{username}'
+    lockout_cache_key = f'lockout_{ip_address}_{username}'
+    lockout_remaining_time_key = f'lockout_expiry_{ip_address}_{username}'
+
+    attempts = cache.get(attempt_count_key,0)+1
+
+    if attempts >= settings.MAX_LOGIN_ATTEMPTS:
+        
+        cache.set(lockout_cache_key, True, settings.LOGIN_ATTEMPT_LOCKOUT_TIME)
+        cache.set(lockout_remaining_time_key, time.time() + settings.LOGIN_ATTEMPT_LOCKOUT_TIME, settings.LOGIN_ATTEMPT_LOCKOUT_TIME)
+        
+        cache.delete(attempt_count_key)
+    else:
+        cache.set(attempt_count_key, attempts, settings.LOGIN_ATTEMPT_LOCKOUT_TIME * 2) 
