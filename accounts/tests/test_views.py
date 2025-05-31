29cea314797cdb41fc8c6da0c9fb5a7ac16f8d3f
alex <alex@gmail.com>
2025-05-30 19:46:26 -0700

    add TestAccountVerifiedToken for test AccountVerificationToken View

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,8 +1,13 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.core.files.uploadedfile import SimpleUploadedFile
+
 from accounts.forms import (
     UserForm,UserUpdateForm,ChangePasswordForm,ForgotPasswordForm
+)
+
+from accounts.models import (
+    CustomUser,AccountVerificationToken,ChangePasswordToken,ForgotPasswordToken
 )
 
 IMAGE_FILE_CONTENT = b'GIF89a\x01\x00\x01\x00\x80\x00\x00\xff\xff\xff\x00\x00\x00!\xf9\x04\x01\x00\x00\x00\x00,\x00\x00\x00\x00\x01\x00\x01\x00\x00\x02\x02D\x01\x00;'
@@ -50,3 +55,33 @@ class TestRegisterView(TestCase):
         
         self.assertTemplateUsed(response,'accounts/account_verified_message.html')
         self.assertEqual(response.status_code,200)
+
+class TestAcountVerifiedToken(TestCase):
+    def setUp(self):
+        self.avatar_file = SimpleUploadedFile(
+            name='test_avatar.gif',
+            content=IMAGE_FILE_CONTENT,
+            content_type='image/gif'
+        )
+
+        self.passport_file = SimpleUploadedFile(
+            name='test_passport.gif',
+            content=IMAGE_FILE_CONTENT,
+            content_type='image/gif'
+        )
+
+        self.user = CustomUser.objects.create_user(
+            username='test', email='test@gmail.com', avatar=self.avatar_file, passport=self.passport_file, 
+            address='test', city='test', country='AF', birth_date='2020-01-02'
+        )
+
+        self.token = AccountVerificationToken.objects.create(
+            user = self.user
+        )
+
+        self.url = reverse('accounts:account_verified_token',args=[self.token.token])
+    
+    def test_get_account_verified_token_view_validate(self):
+        response = self.client.get(self.url)
+        self.assertRedirects(response,reverse('accounts:login'))
+        self.assertEqual(response.status_code,302)
