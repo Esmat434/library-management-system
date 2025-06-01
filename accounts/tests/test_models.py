e7710a97ba9a414ef9789da8b22f7efebb908968
alex <alex@gmail.com>
2025-05-31 21:09:11 -0700

    add TestForgotPasswordTokenModel for test model of ForgotPasswordToken

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -58,3 +58,24 @@ class TestChangePasswordTokenModel(TestCase):
     
     def test_is_expired_method_validate(self):
         self.assertFalse(self.token.is_expired())
+
+class TestForgotPasswordTokenModel(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username='test', email='test@gmail.com', avatar='', passport='', 
+            address='test', city='test', country='AF', birth_date='2020-01-02', email_verified=True,
+            password='Test12345%'
+        )
+
+        self.token = ForgotPasswordToken.objects.create(
+            user=self.user
+        )
+    
+    def test_forgot_password_token_model_validate(self):
+        token_instance = ForgotPasswordToken.objects.get(user=self.user)
+
+        self.assertEqual(self.token.token,token_instance.token)
+        self.assertEqual(self.token.user,self.user)
+    
+    def test_is_expired_method_validate(self):
+        self.assertFalse(self.token.is_expired())
