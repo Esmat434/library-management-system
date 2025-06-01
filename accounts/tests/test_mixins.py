9a232fb24d28f5ef0f04bed145e14a5770623f29
alex <alex@gmail.com>
2025-05-31 21:43:23 -0700

    add TestLogoutRequiredMixin for test LogoutRequiredMixin on RegisterView

--- accounts/tests/test_mixins.py
+++ accounts/tests/test_mixins.py
@@ -27,3 +27,25 @@ class TestLoginRequiredMixin(TestCase):
         self.assertTemplateUsed(response,'accounts/profile.html')
         self.assertEqual(response.status_code,200)
         
+class TestLogoutRequiredMixin(TestCase):
+    def setUp(self):
+        self.url = reverse('accounts:register')
+
+        self.user = User.objects.create_user(
+            username='test', email='test@gmail.com', avatar='', passport='', 
+            address='test', city='test', country='AF', birth_date='2020-01-02', email_verified=True,
+            password='Test12345%'
+        )
+    
+    def test_redirect_if_login(self):
+        self.client.login(username='test', password='Test12345%')
+        response = self.client.get(self.url)
+
+        self.assertEqual(response.status_code,302)
+    
+    def test_render_if_not_login(self):
+        response = self.client.get(self.url)
+
+        self.assertTemplateUsed(response,'accounts/register.html')
+        self.assertEqual(response.status_code,200)
+        
