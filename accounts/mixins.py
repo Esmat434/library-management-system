d75a7c033ea45bfb2f55dc72001fae603e0cc02c
alex <alex@gmail.com>
2025-05-31 20:30:30 -0700

    fix mixins.py

--- accounts/mixins.py
+++ accounts/mixins.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render,redirect
 from django.urls import reverse
 from django.contrib.auth.mixins import AccessMixin
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 class LoginRequiredMixin(AccessMixin):
     def dispatch(self,request,*args,**kwargs):
@@ -14,8 +18,17 @@ class LogoutRequiredMixin(AccessMixin):
             return redirect('/')
         return super().dispatch(request,*args,**kwargs)
 
-class AccountVerifiedMixin(LoginRequiredMixin):
-    def dispatch(self, request, *args, **kwargs):
-        if not getattr(request.user,'email_verified',False):
-            return render(request,'accounts/account_verified_message.html')
-        return super().dispatch(request, *args, **kwargs)
+class AccountVerifiedBeforeLoginMixin:
+    def dispatch(self,request, *args, **kwargs):
+        if request.method == 'POST':
+            username = request.POST.get('username')
+
+            try:
+                user = User.objects.get(username = username)
+
+                if not getattr(user,'email_verified',False):
+                    messages.error(request, 'Please verify your email before logging in.')
+                    return render(request, 'accounts/account_verified_message.html')
+            except User.DoesNotExist:
+                pass
+        return super().dispatch(request,*args,**kwargs)
