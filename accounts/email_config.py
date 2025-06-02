386f4e8b586d7686964d51cd93963bbbcfe00813
alex <alex@gmail.com>
2025-06-01 18:22:33 -0700

    change template path to accounts/email.html

--- accounts/email_config.py
+++ accounts/email_config.py
@@ -3,7 +3,7 @@ from django.template.loader import render_to_string
 from django.conf import settings
 
 def send_verification_mail(to,subject,token):
-    html_content = render_to_string('email.html',{'token':token})
+    html_content = render_to_string('accounts/email.html',{'token':token})
     email = EmailMessage(subject,html_content,settings.EMAIL_HOST_USER,[to])
     email.content_subtype = 'html'
 
