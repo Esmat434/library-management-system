ab6370e95249065b43c95d80ff8834a9d22df736
alex <alex@gmail.com>
2025-06-02 21:53:16 -0700

    fix something in urlpatterns

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,12 +11,12 @@ urlpatterns = [
     path('register/',RegisterView.as_view(),name='register'),
     path('login/',LoginView.as_view(),name='login'),
     path('account_verified/<uuid:token>/',AccountVerifiedTokenView.as_view(),name='account_verified_token'),
-    path('resend_account_token/',AccountVerifiedResendTokenView.as_view(),name='resend_account_token'),
+    path('account_resend_token/',AccountVerifiedResendTokenView.as_view(),name='account_resend_token'),
     path('Logout/',LogoutView.as_view(),name='logout'),
     path('profile/',ProfileView.as_view(),name='profile'),
     path('profile/update/',ProfileUpdateView.as_view(),name='profile_update'),
     path('change_password_token/',ChangePasswordTokenView.as_view(),name='change_password_token'),
     path('change_password/<uuid:token>/',ChangePasswordView.as_view(),name='change_password'),
-    path('forgot_password_token/',ForgotPasswordResendLinkView.as_view(),name='forgot_password_token'),
+    path('forgot_password_token/',ForgotPasswordResendLinkView.as_view(),name='forgot_password_resend_token'),
     path('forgot_password/<uuid:token>/',ForgotPasswordView.as_view(),name='forgot_password')
 ]
