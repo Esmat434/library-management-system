e36bfb9144effa7679dc8a978e93f374280b994a
alex <alex@gmail.com>
2025-06-04 23:04:40 -0700

    add __str__ in Book Model

--- books/models.py
+++ books/models.py
@@ -19,6 +19,9 @@ class Book(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
 class Category(models.Model):
     name = models.CharField(max_length=155)
     description = models.TextField(blank=True)
