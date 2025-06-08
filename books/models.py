4600de5651e96c5eaf3aebe5a41ec8b9722a1fbc
alex <alex@gmail.com>
2025-06-07 22:09:44 -0700

    fix related_name

--- books/models.py
+++ books/models.py
@@ -94,8 +94,8 @@ class BorrowTransaction(models.Model):
         return f"borrow {self.book_copy.book.title} to {self.user.username}"
     
 class Reservation(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-    book = models.ForeignKey(Book, on_delete=models.PROTECT, related_name='book')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reserve_user')
+    book = models.ForeignKey(Book, on_delete=models.PROTECT, related_name='reserve_book')
     is_active = models.BooleanField(default=False)
     reservation_date = models.DateTimeField(auto_now_add=True)
 
@@ -108,7 +108,7 @@ class Reservation(models.Model):
         return f"{self.user.username} reserved {self.book.title}"
 
 class Fine(models.Model):
-    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='user')
+    user = models.ForeignKey(User, on_delete=models.PROTECT, related_name='fine_user')
     borrow_transaction = models.ForeignKey(BorrowTransaction, on_delete=models.PROTECT, related_name='borrow_transaction')
     amount = models.DecimalField(max_digits=8,decimal_places=2)
     is_paid = models.BooleanField(default=False)
