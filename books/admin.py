727c6146bc04d3fca08292f05212c8cc1dfdbf8b
alex <alex@gmail.com>
2025-06-07 19:11:53 -0700

    add BookAdmin in admin.py

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import (
+    Book,Category,Author,Publisher,BookCopy,BorrowTransaction,Reservation,Fine,
+)
 # Register your models here.
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['id','title','category','author','publisher','isbn','total_copies','available_copies','published_date']
+    list_filter = ['category','author','publisher']
+    search_fields = ['id','title','isbn']
+    ordering = ['-id']
