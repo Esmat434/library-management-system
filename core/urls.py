bb9d961722b20d91cb7e08dcb382ef2bef457a9c
alex <alex@gmail.com>
2025-06-11 21:39:31 -0700

    add app_name in urls.py

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from .views import (
     AboutUsView,ContactUsView
 )
 
+app_name='core'
+
 urlpatterns = [
     path('about/',AboutUsView.as_view(),name='about_us'),
     path('contact/',ContactUsView.as_view(),name='contact_us')
