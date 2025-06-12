2d9d1de80efc0723a0124206ddcada2ba1b3a42d
alex <alex@gmail.com>
2025-06-11 22:34:37 -0700

    create AboutUsView and ContactUsView in views.py

--- core/views.py
+++ core/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.views import View
+
+from .forms import (
+    ContactCreationForm
+)
 
 # Create your views here.
+
+class AboutUsView(View):
+    def get(self,request):
+        return render(request,'core/about_us.html')
+
+class ContactUsView(View):
+    def get(self,request):
+        return render(request,'core/contact_us.html')
+    
+    def post(self,request):
+        form = ContactCreationForm(request.POST)
+        if form.is_valid():
+            contact = form.save(commit=False)
+            contact.user = request.user if request.user.is_authenticated else None
+            contact.save()
+            messages.success(request,'Your message successfully sended.')
+            return redirect('books:home')
+        messages.error(request,'Your data was invalid.')
+        return render(request,'core/contact_us.html')
