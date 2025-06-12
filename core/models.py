0070b3263b18dc67f6a39fa65babe44182793354
alex <alex@gmail.com>
2025-06-11 20:31:38 -0700

    create Contact model in books app

--- core/models.py
+++ core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
-
+from django.contrib.auth import get_user_model
 # Create your models here.
+
+User = get_user_model()
+
+class Contact(models.Model):
+    user = models.ForeignKey(User,on_delete=models.SET_NULL,null=True,blank=True)
+    username = models.CharField(max_length=150)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=50)
+    title = models.CharField(max_length=150)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.username
