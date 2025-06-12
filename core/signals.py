6234079ec4d37b41399a2913c938882ea47a37b6
alex <alex@gmail.com>
2025-06-11 22:54:10 -0700

    add signals in core app

---
+++ core/signals.py
@@ -0,0 +1,15 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .email import (
+    send_message_email
+)
+
+from .models import (
+    Contact
+)
+
+@receiver(post_save,sender=Contact)
+def send_contact_message_notification(sender,created,instance,**kwargs):
+    if created:
+        send_message_email(Contact.email,'Contact US',instance.title,instance.message,instance.username)
