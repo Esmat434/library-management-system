9f28414b7e35251434fe79e689061f7c1fe7798e
alex <alex@gmail.com>
2025-06-11 19:55:19 -0700

    add test_urls in books app

---
+++ books/tests/test_urls.py
@@ -0,0 +1,54 @@
+from django.test import SimpleTestCase
+from django.urls import reverse
+
+class TestUrls(SimpleTestCase):
+    
+    def test_book_list_url_is_reversed(self):
+        url_name = 'books:home'
+        url_path = reverse(url_name)
+        self.assertEqual(url_path,'/books/')
+        
+    def test_book_detail_url_is_reversed(self):
+        url_name = 'books:book_detail'
+        url_path = reverse(url_name,args=['test'])
+        self.assertEqual(url_path,'/books/test/')
+
+    def test_book_filter_by_author_url_is_reversed(self):
+        url_name = 'books:book_filter_by_author'
+        url_path = reverse(url_name)
+        self.assertEqual(url_path,'/books/filter/author/')
+    
+    def test_book_filter_by_category_url_is_reversed(self):
+        url_name = 'books:book_filter_by_category'
+        url_path = reverse(url_name)
+        self.assertEqual(url_path,'/books/filter/category/')
+    
+    def test_book_filter_by_publisher_url_is_reversed(self):
+        url_name = 'books:book_filter_by_publisher'
+        url_path = reverse(url_name)
+        self.assertEqual(url_path,'/books/filter/publisher/')
+    
+    def test_borrow_url_is_reversed(self):
+        url_name = 'books:borrow_transaction_list'
+        url_path = reverse(url_name)
+        self.assertEqual(url_path,'/borrow/')
+    
+    def test_borrow_create_url_is_reversed(self):
+        url_name = 'books:borrow_transaction_create'
+        url_path = reverse(url_name,args=['test'])
+        self.assertEqual(url_path,'/borrow/test/')
+    
+    def test_reserved_url_is_reversed(self):
+        url_name = 'books:reserved_list'
+        url_path = reverse(url_name)
+        self.assertEqual(url_path,'/reserved/')
+    
+    def test_reserved_create_url_is_reversed(self):
+        url_name = 'books:reserved_create'
+        url_path = reverse(url_name,args=['test'])
+        self.assertEqual(url_path,'/reserved/test/')
+    
+    def test_return_borrow_url_is_reversed(self):
+        url_name = 'books:return_borrow'
+        url_path = reverse(url_name,args=[1])
+        self.assertEqual(url_path,'/return_borrow/1/')
