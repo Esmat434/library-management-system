19652d65dacd200e90633c0ad122edbe8e3aba66
alex <alex@gmail.com>
2025-06-19 00:07:05 -0700

    add dashboard in urls

--- LibraryManagementSystem/urls.py
+++ LibraryManagementSystem/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls',namespace='accounts')),
     path('',include('books.urls',namespace='books')),
-    path('',include('core.urls',namespace='core')),
+    path('core/',include('core.urls',namespace='core')),
     path('dashboard/',include('dashboard.urls',namespace='dashboard')),
 ]
 
