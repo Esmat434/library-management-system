fc80677d09619b40cf772b839983d7d84c2d6ab7
alex <alex@gmail.com>
2025-06-18 23:56:26 -0700

    fix something in email in books app

--- books/email.py
+++ books/email.py
@@ -2,8 +2,8 @@ from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
 from django.conf import settings
 
-def send_notification_email(to,subject,book_title,username):
-    html_content = render_to_string('books/email.html',{'title':book_title,'username':username})
+def send_notification_email(to,subject,instance,title):
+    html_content = render_to_string('books/email.html',{'instance':instance,'subject':subject,'title':title})
     email = EmailMessage(subject,html_content,settings.EMAIL_HOST_USER,to=[to])
     email.content_subtype = 'html'
 
