c2b053dbc8a6dda3f052e1e1838b4355d0ecdcda
alex <alex@gmail.com>
2025-06-20 18:53:54 -0700

    add form in ContactUsView get method

--- core/views.py
+++ core/views.py
@@ -14,7 +14,8 @@ class AboutUsView(View):
 
 class ContactUsView(View):
     def get(self,request):
-        return render(request,'core/contact_us.html')
+        form = ContactCreationForm()
+        return render(request,'core/contact_us.html',{'form':form})
     
     def post(self,request):
         form = ContactCreationForm(request.POST)
@@ -22,7 +23,7 @@ class ContactUsView(View):
             contact = form.save(commit=False)
             contact.user = request.user if request.user.is_authenticated else None
             contact.save()
-            messages.success(request,'Your message successfully sended.')
+            messages.success(request,'Your contact message successfully sended.')
             return redirect('books:home')
         messages.error(request,'Your data was invalid.')
         return render(request,'core/contact_us.html')
